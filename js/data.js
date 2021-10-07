ab39ed992647a212a651890b504dfeca8f6afbba
karla-walker <indiablonix@gmail.com>
2021-10-07 12:04:29 -0700

    Add treehouse student data

--- js/data.js
+++ js/data.js
@@ -2,10 +2,19 @@
 
 const data = [
   {
-    url: "https://teamtreehouse.com/dlv.json",
-    name: "Lee Vaughn",
-    profile_name: "dlv",
-    gravatar_url: "https://uploads.teamtreehouse.com/production/profile-photos/9205552/avatar_lee.jpg",
-    points: 41374
-  }
+    url: 'https://teamtreehouse.com/indiablonix.json',
+    name: 'Karla Walker',
+    profile_name: 'indiablonix',
+    gravatar_url:
+      'https://uploads.teamtreehouse.com/production/profile-photos/6689732/avatar_IMG_20201001_143040_444.jpg',
+    points: 15580,
+  },
+  {
+    url: 'https://teamtreehouse.com/dlv.json',
+    name: 'Lee Vaughn',
+    profile_name: 'dlv',
+    gravatar_url:
+      'https://uploads.teamtreehouse.com/production/profile-photos/9205552/avatar_lee.jpg',
+    points: 41374,
+  },
 ];
